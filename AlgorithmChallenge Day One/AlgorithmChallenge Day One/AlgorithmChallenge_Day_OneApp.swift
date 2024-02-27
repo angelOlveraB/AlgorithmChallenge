327e0b153a0d0b023adb0da409c8bb0d2caeb6ef
Angel Obed Olvera Borjas <angel.olvera@globant.com>
2024-02-27 11:31:36 -0600

    Project Creation

---
+++ AlgorithmChallenge Day One/AlgorithmChallenge Day One/AlgorithmChallenge_Day_OneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmChallenge_Day_OneApp.swift
+//  AlgorithmChallenge Day One
+//
+//  Created by Angel Olvera on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmChallenge_Day_OneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
